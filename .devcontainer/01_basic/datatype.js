c08fd60aad8a54fdeff2c158093effef236c20c3
karan devid <149402058+karandevid@users.noreply.github.com>
2024-07-18 05:01:38 +0000

    variable.js And type.js

---
+++ .devcontainer/01_basic/datatype.js
@@ -0,0 +1,22 @@
+// "use strict"  treat all code use as a new versio
+// alert(3+3)    we are using node js ,not browser
+
+console.log(3+3)
+console.log("karan devid")
+
+let age=18
+let isLoggedIn=false
+let state =null
+
+// Number=> 2 to power 23
+// bigint 
+// String=> ""
+// Boolean=>true/false
+// undefined=>undefined
+// Symbol=>unique
+
+// Object
+
+console.log(typeof "karan devid")
+
+
