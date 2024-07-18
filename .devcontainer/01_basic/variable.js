668189765b0da578949537e7340ec9e424cf1d54
karan devid <149402058+karandevid@users.noreply.github.com>
2024-07-18 08:10:27 +0000

    variable.js

---
+++ .devcontainer/01_basic/variable.js
@@ -0,0 +1,8 @@
+const accountId=12345
+let accountEmail="kko24@gmail.com"
+var accountPassword="12344"
+accountCity="hazaribagh"
+
+
+// accountId=2  //not allowed
+console.table([accountEmail,accountId,accountCity])
