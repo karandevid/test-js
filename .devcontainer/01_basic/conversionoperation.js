20c79d41efaf526710f015ff084bc317d7a5441f
karan devid <149402058+karandevid@users.noreply.github.com>
2024-07-18 08:11:18 +0000

    conversionoperation.js

---
+++ .devcontainer/01_basic/conversionoperation.js
@@ -0,0 +1,38 @@
+let score="33ab"
+
+console.log(typeof score)
+console.log((score))
+
+let ValueInNunmber=Number(score)
+console.log(typeof ValueInNumber)
+
+/*"33"=>33
+"33abc"=>Nan
+true=>1; ,false=>0;
+
+*/
+
+let isLoggedIn =1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+/* 1=>true; o=>false;
+""=>false
+"karan devid"=>true
+*/
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
+
+/*operation*/
+
+let value=3
+let nagvalue= -value
+console.log(nagvalue);
+
+console.log("1"+2)
+console.log(1+"2")
+console.log(1+2+"2")
